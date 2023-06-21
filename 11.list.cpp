97f567b02d9c67d6eb0c766df5cfe0a6b9a307ed
coolboy973 <1824280451@qq.com>
2023-06-21 11:26:21 +0800

    11

--- 11.list.cpp
+++ 11.list.cpp
@@ -7,6 +7,9 @@ void printList(list <int>&L){
     }
     cout<<endl;
 }
+bool compare(int v1,int v2){
+    return v1 > v2;
+}
 int main(){
     // (1)List容器构造
     // list <int> L;
@@ -37,35 +40,55 @@ int main(){
     // printList(l);
 
     //(3)list容器插入和删除
-    list<int> l1;
-    //尾插
-    l1.push_back(10);
-    l1.push_back(20);
-    l1.push_back(30);
-    printList(l1);//10,20,30
-    //头插
-    l1.push_front(100);
-    l1.push_front(200);
-    l1.push_front(100);
-    printList(l1);//100 200 100 10 20 30
-    //尾删
-    l1.pop_back();
-    printList(l1);//100 200 100 10 20
-    //头删
-    l1.pop_front();
-    printList(l1);//200 100 10 20
+    // list<int> l1;
+    // //尾插
+    // l1.push_back(10);
+    // l1.push_back(20);
+    // l1.push_back(30);
+    // printList(l1);//10,20,30
+    // //头插
+    // l1.push_front(100);
+    // l1.push_front(200);
+    // l1.push_front(100);
+    // printList(l1);//100 200 100 10 20 30
+    // //尾删
+    // l1.pop_back();
+    // printList(l1);//100 200 100 10 20
+    // //头删
+    // l1.pop_front();
+    // printList(l1);//200 100 10 20
 
-    //任意位置插入
-    list<int>::iterator it = l1.begin();
-    l1.insert(++it,50);
-    printList(l1);//200 50 100 10 20
+    // //任意位置插入
+    // list<int>::iterator it = l1.begin();
+    // l1.insert(++it,50);
+    // printList(l1);//200 50 100 10 20
      
-    it = l1.begin();
-    l1.insert(++it,4,10);
-    printList(l1);//200 10 10 10 10 50 100 10 20
+    // it = l1.begin();
+    // l1.insert(++it,4,10);
+    // printList(l1);//200 10 10 10 10 50 100 10 20
 
-    // 删除
-    l1.remove(10);
-    printList(l1);//200 50 100 20
+    // // 删除
+    // l1.remove(10);
+    // printList(l1);//200 50 100 20
+    
+    // (4)list容器数据存取
+    // list<int> l1;
+    // l1.push_back(10);
+    // l1.push_back(20);
+    // l1.push_back(30);
+    // cout<<"第一个元素为: "<<l1.front()<<endl; //10
+    // cout<<"最后一个元素为: "<<l1.back()<<endl; //30
+
+    //(5)list容器反转和排序
+    list<int> l1;
+    l1.push_back(20);
+    l1.push_back(10);
+    l1.push_back(30);
+    l1.reverse();
+    printList(l1);//30 10 20
+    l1.sort();
+    printList(l1);//10 20 30默认是升序
+    l1.sort(compare);
+    printList(l1); // 30 20 10
     return 0;
 }
---
+++ 12.list_mysort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include <algorithm>
+using namespace std;
+class Person{
+public:
+    Person(string name,int age , int height){
+        this->name = name;
+        this->age = age;
+        this->height = height;
+    }
+    string name;
+    int age;
+    int height;
+};
+void printlist(list<Person>const &l){
+    for(list<Person>::const_iterator it = l.begin();it!=l.end();it++){
+        cout<<"姓名："<<it->name<<" 年龄："<<it->age <<" 身高: "<<it->height<<endl;
+    }
+}
+bool my_compare(Person &l1,Person &l2){
+    if(l1.age==l2.age){
+        return l1.height>l2.height;
+    }
+    else{
+        return l1.age<l2.age;
+    }
+}
+int main(){
+    list<Person> l;
+    Person p1("张三",50,174);
+    Person p2("李四",50,168);
+    Person p3("王五",30,194);
+    Person p4("老刘",23,184);
+    Person p5("张清",19,192);
+    l.push_back(p1);
+    l.push_back(p2);
+    l.push_back(p3);
+    l.push_back(p4);
+    l.push_back(p5);
+    cout<<"排序前: "<<endl;
+    printlist(l);
+    l.sort(my_compare);
+    cout<<"排序后: "<<endl;
+    printlist(l);
+    return 0;
+}
