11a9e0916e24ce4442b092be400d5a1e5e04c7fb
tpj <1824280451@qq.com>
2023-07-08 17:08:56 +0800

    2023/7/8

--- leetcode/5.5.removenthformend.cpp
+++ leetcode/5.5.removenthformend.cpp
@@ -39,23 +39,53 @@ void printlist(ListNode *head){
         }
     }
 }
+class Solution1 {
+public:
+    ListNode* reverselist(ListNode* head) {
+        ListNode*pre = nullptr;
+        ListNode*start = head;
+        ListNode*end;
+        while(start){
+            end = start->next;
+            start->next = pre;
+            pre = start;
+            start=end;
+        }
+        return pre;
+    }
+};
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-
+        ListNode*p = head;
+        for(int i=0;i<n-1;i++){
+            p=p->next;
+        }
+        ListNode*temp = p->next;
+        p->next = temp->next;
+        delete temp;
+        return head;
     }
 };
 int main(){
-    int nums[4]={1,2,3,4};
+    int nums[5]={1,2,3,4,5};
     int n = 2;
     int len = sizeof(nums)/sizeof(nums[0]);
     ListNode *head = createhead(nums,len);
     cout<<"删除前:"<<endl;
     printlist(head);
     cout<<endl;
+    Solution1 s1;
+    cout<<"翻转"<<endl;
+    head = s1.reverselist(head);
+    printlist(head);
+    cout<<endl;
     Solution s;
     head = s.removeNthFromEnd(head,n);
     cout<<"删除后:"<<endl;
+    printlist(head);
+    cout<<"翻转"<<endl;
+    head = s1.reverselist(head);
     printlist(head);
     cout<<endl;
     return 0;
